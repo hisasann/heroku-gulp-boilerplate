65c2f4cd6e18f00dbf99e50146c02e989ab39064
hisasann <hisasann25@gmail.com>
2015-04-09 13:50:51 +0900

    babel環境を整える
    Signed-off-by: hisasann <hisasann25@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,23 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var server = require('gulp-webserver');
 
-gulp.task('default', [], function() {
-  console.log('defalut');
+gulp.task('build', [], function() {
+  console.log('build');
+
+  gulp.src(['source/javascripts/*.js'])
+    .pipe(babel())
+    .pipe(gulp.dest('build/javascripts'));
 });
 
 // gulp系のserverはlocalhostとして立ち上げるからかうまくheroku側で起動できない
-//gulp.task('server', [], function() {
-//  console.log('server');
-//
-//  gulp.src('build')
-//    .pipe(server({
-//      livereload: true,
-//      port: process.env.PORT || 4567, // localhost:4567
-//      open: true
-//    }));
-//});
+gulp.task('server', ['build'], function() {
+  console.log('server');
+
+  gulp.src('build')
+    .pipe(server({
+      livereload: true,
+      port: process.env.PORT || 4567, // localhost:4567
+      open: true
+    }));
+});
 
