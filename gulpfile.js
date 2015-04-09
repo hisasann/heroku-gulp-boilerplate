87d524044450f42a62e0385fde80b2a290f67a01
hisasann <hisasann25@gmail.com>
2015-04-09 13:32:24 +0900

    gulp defalutを一応追加してみる
    Signed-off-by: hisasann <hisasann25@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var server = require('gulp-webserver');
 
+gulp.task('default', [], function() {
+  console.log('defalut');
+});
+
 gulp.task('server', [], function() {
+  console.log('server');
+
   gulp.src('build')
     .pipe(server({
       livereload: true,
